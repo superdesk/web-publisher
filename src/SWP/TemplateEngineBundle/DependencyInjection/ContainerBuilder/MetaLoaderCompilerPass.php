cb4a10d24320e9c61f3180c56626a2e0baa7946b
Pawel Mikolajczuk <mikolajczuk.private@gmail.com>
2015-07-30 10:22:24 +0200

    add proper header

--- src/SWP/TemplateEngineBundle/DependencyInjection/ContainerBuilder/MetaLoaderCompilerPass.php
+++ src/SWP/TemplateEngineBundle/DependencyInjection/ContainerBuilder/MetaLoaderCompilerPass.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the Superdesk Web Publisher Template Engine Bundle
+ *
+ * Copyright 2015 Sourcefabric z.u. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2015 Sourcefabric z.ú.
+ * @license http://www.superdesk.org/license
+ */
 
 namespace SWP\TemplateEngineBundle\DependencyInjection\ContainerBuilder;
 
