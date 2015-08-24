4bcdc6291086812676b74c409cb12339080ddbdb
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2015-08-24 20:35:00 +0200

    improve theme devices templates tests

--- src/SWP/WebRendererBundle/Tests/Controller/DefaultControllerTest.php
+++ src/SWP/WebRendererBundle/Tests/Controller/DefaultControllerTest.php
@@ -19,43 +19,24 @@ class DefaultControllerTest extends WebTestCase
         $filesystem->remove(__DIR__.'/../../../../../app/Resources/themes/theme_test');
     }
 
-    public function testIndexOnPhone()
+    public function testIndexOnDevices()
     {
+        $devices = [
+            'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5' => 'html:contains("theme_test/phone/index.html.twig")',
+            'Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5' => 'html:contains("theme_test/tablet/index.html.twig")',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:8.0.1) Gecko/20100101 Firefox/8.0.1 FirePHP/0.6' => 'html:contains("theme_test/desktop/index.html.twig")',
+        ];
+
         $client = static::createClient();
-        $client->setServerParameters(array(
-            'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
-        ));
+        foreach ($devices as $userAgent => $filter) {
+            $client->setServerParameters(array(
+                'HTTP_USER_AGENT' => $userAgent,
+            ));
+            $crawler = $client->request('GET', '/');
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("theme_test/phone/index.html.twig")')->count() > 0);
-    }
-
-    public function testIndexOnTablet()
-    {
-        $client = static::createClient();
-        $client->setServerParameters(array(
-            'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
-        ));
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("theme_test/tablet/index.html.twig")')->count() > 0);
-    }
-
-    public function testIndexOnDesktop()
-    {
-        $client = static::createClient();
-        $client->setServerParameters(array(
-            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:8.0.1) Gecko/20100101 Firefox/8.0.1 FirePHP/0.6',
-        ));
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("theme_test/desktop/index.html.twig")')->count() > 0);
+            $this->assertEquals(200, $client->getResponse()->getStatusCode());
+            $this->assertTrue($crawler->filter($filter)->count() > 0);
+        }
     }
 
     public function testIndexWithoutUserAgent()
