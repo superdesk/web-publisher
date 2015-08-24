8e65a2f063b8f9a0115ce108854dbb251e8c8dfb
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2015-08-24 20:52:26 +0200

    improve theme devices templates tests

--- src/SWP/WebRendererBundle/Tests/Controller/DefaultControllerTest.php
+++ src/SWP/WebRendererBundle/Tests/Controller/DefaultControllerTest.php
@@ -25,39 +25,27 @@ class DefaultControllerTest extends WebTestCase
             'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5' => 'html:contains("theme_test/phone/index.html.twig")',
             'Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5' => 'html:contains("theme_test/tablet/index.html.twig")',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:8.0.1) Gecko/20100101 Firefox/8.0.1 FirePHP/0.6' => 'html:contains("theme_test/desktop/index.html.twig")',
+            'no_agent_0' => 'html:contains("theme_test/desktop/index.html.twig")',
+            'no_agent_1' => 'html:contains("Homepage theme_test")',
         ];
 
         $client = static::createClient();
         foreach ($devices as $userAgent => $filter) {
-            $client->setServerParameters(array(
-                'HTTP_USER_AGENT' => $userAgent,
-            ));
+            if (!in_array($userAgent, ['no_agent_0', 'no_agent_1'])) {
+                $client->setServerParameters(array(
+                    'HTTP_USER_AGENT' => $userAgent,
+                ));
+            }
+
+            if ($userAgent === 'no_agent_1') {
+                $filesystem = new Filesystem();
+                $filesystem->remove(__DIR__.'/../../../../../app/Resources/themes/theme_test/desktop/index.html.twig');
+            }
+
             $crawler = $client->request('GET', '/');
 
             $this->assertEquals(200, $client->getResponse()->getStatusCode());
             $this->assertTrue($crawler->filter($filter)->count() > 0);
         }
-    }
-
-    public function testIndexWithoutUserAgent()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("theme_test/desktop/index.html.twig")')->count() > 0);
-    }
-
-    public function testIndexWithoutDeviceTemplate()
-    {
-        $filesystem = new Filesystem();
-        $filesystem->remove(__DIR__.'/../../../../../app/Resources/themes/theme_test/desktop/index.html.twig');
-
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage theme_test")')->count() > 0);
     }
 }
