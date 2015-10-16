7c09ee2c182d04d785fa604bc58bd6b6768c141d
Rafał Muszyński <rmuszynski1@gmail.com>
2015-10-16 16:02:56 +0200

    added possibility to cache Autoloader with APC or XCache

--- web/app.php
+++ web/app.php
@@ -1,11 +1,23 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\ClassLoader\XcacheClassLoader;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
+
+if (extension_loaded('apc') && ini_get('apc.enabled')) {
+    $loader = new ApcClassLoader('superdesk_webpublisher', $loader);
+    $loader->register(true);
+}
+
+if (extension_loaded('xcache') && ini_get('xcache.enabled')) {
+    $loader = new XcacheClassLoader('superdesk_webpublisher', $loader);
+    $loader->register(true);
+}
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
