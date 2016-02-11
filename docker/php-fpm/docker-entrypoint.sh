4aab479bce465e3e9473ae2e7346bc6979541c53
Rafał Muszyński <rmuszynski1@gmail.com>
2016-02-11 15:49:38 +0100

    rename webrenderer to webpublisher

--- docker/php-fpm/docker-entrypoint.sh
+++ docker/php-fpm/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 if [ "$1" = 'composer' ]; then
     # install composer and install PHP dependencies
-    if [ ! -f /var/www/webrenderer/composer.phar ]; then
-        cd /var/www/webrenderer && curl -Ssk https://getcomposer.org/installer | php
+    if [ ! -f /var/www/webpublisher/composer.phar ]; then
+        cd /var/www/webpublisher && curl -Ssk https://getcomposer.org/installer | php
     fi
-    cd /var/www/webrenderer && /usr/bin/php composer.phar install
-    #cd /var/www/webrenderer && /usr/bin/php composer.phar install --no-dev
-    chown -R www-data:1000 /var/www/webrenderer
+    cd /var/www/webpublisher && /usr/bin/php composer.phar install
+    #cd /var/www/webpublisher && /usr/bin/php composer.phar install --no-dev
+    chown -R www-data:1000 /var/www/webpublisher
 
     /usr/sbin/php5-fpm -F
 
