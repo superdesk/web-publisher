09cef10d4ba5b205c912dc969045bc4aba5e5a3b
Rafał Muszyński <rmuszynski1@gmail.com>
2016-05-23 13:00:45 +0200

    [MultiTenancy] SWP-120 - Create Twig global to get current tenant in twig templates

---
+++ src/SWP/Bundle/MultiTenancyBundle/Twig/MultiTenancyExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the Superdesk Web Publisher MultiTenancy Component.
+ *
+ * Copyright 2016 Sourcefabric z.u. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2016 Sourcefabric z.ú.
+ * @license http://www.superdesk.org/license
+ */
+
+namespace SWP\Bundle\MultiTenancyBundle\Twig;
+
+use SWP\Component\MultiTenancy\Context\TenantContextInterface;
+
+class MultiTenancyExtension extends \Twig_Extension
+{
+    /**
+     * @var TenantContextInterface
+     */
+    protected $tenantContext;
+
+    /**
+     * @param TenantContextInterface $tenantContext
+     */
+    public function __construct(TenantContextInterface $tenantContext)
+    {
+        $this->tenantContext = $tenantContext;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGlobals()
+    {
+        return [
+            'organization' => $this->tenantContext->getTenant(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'swp_multi_tenancy';
+    }
+}
---
+++ src/SWP/Bundle/MultiTenancyBundle/spec/Twig/MultiTenancyExtensionSpec.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of the Superdesk Web Publisher MultiTenancy Component.
+ *
+ * Copyright 2016 Sourcefabric z.u. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2016 Sourcefabric z.ú.
+ * @license http://www.superdesk.org/license
+ */
+
+namespace spec\SWP\Bundle\MultiTenancyBundle\Twig;
+
+use PhpSpec\ObjectBehavior;
+use SWP\Bundle\MultiTenancyBundle\Twig\MultiTenancyExtension;
+use SWP\Component\MultiTenancy\Context\TenantContextInterface;
+use SWP\Component\MultiTenancy\Model\TenantInterface;
+use Twig_Extension;
+
+class MultiTenancyExtensionSpec extends ObjectBehavior
+{
+    function let(TenantContextInterface $tenantContext)
+    {
+        $this->beConstructedWith($tenantContext);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(MultiTenancyExtension::class);
+    }
+
+    function it_is_a_twig_extension()
+    {
+        $this->shouldHaveType(Twig_Extension::class);
+    }
+
+    function it_should_return_global_variables(TenantInterface $tenant, TenantContextInterface $tenantContext)
+    {
+        $tenant->getSubdomain()->willReturn('example');
+        $tenant->getName()->willReturn('example tenant');
+        $tenantContext->getTenant()->shouldBeCalled()->willReturn($tenant);
+
+        $globals = [
+            'organization' => $tenant
+        ];
+
+        $this->getGlobals()->shouldReturn($globals);
+    }
+
+    function it_should_have_a_name()
+    {
+        $this->getName()->shouldReturn('swp_multi_tenancy');
+    }
+}
