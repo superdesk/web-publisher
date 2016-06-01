1c6c43169588b5161f82f034404015dca5a384f3
Rafał Muszyński <rmuszynski1@gmail.com>
2016-06-01 12:09:34 +0200

    [Composer][Travis] Drop PHP version 5.5, travis improvements (#73)
    * [Composer] Drop PHP version 5.5
    
    * [Travis] Enabled redis doctrine cache driver for PHP 7
    
    [Composer] identation fix
    
    [Travis] Enabled redis extension for PHP 7
    
    * [StyleCI] Applied code style fixes

--- src/SWP/Bundle/BridgeBundle/Controller/BridgeController.php
+++ src/SWP/Bundle/BridgeBundle/Controller/BridgeController.php
@@ -11,7 +11,6 @@
  * @copyright 2015 Sourcefabric z.ú.
  * @license http://www.superdesk.org/license
  */
-
 namespace SWP\Bundle\BridgeBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
--- src/SWP/Bundle/BridgeBundle/spec/Client/GuzzleApiClientSpec.php
+++ src/SWP/Bundle/BridgeBundle/spec/Client/GuzzleApiClientSpec.php
@@ -11,7 +11,6 @@
  * @copyright 2015 Sourcefabric z.ú.
  * @license http://www.superdesk.org/license
  */
-
 namespace spec\SWP\Bundle\BridgeBundle\Client;
 
 use PhpSpec\ObjectBehavior;
