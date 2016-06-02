1cd1de9d177e5869197691c4b3ab2d80fc4a4397
Rafał Muszyński <rmuszynski1@gmail.com>
2016-06-02 14:09:54 +0200

    [Common] Added common interfaces and traits

---
+++ src/SWP/Component/Common/Model/TranslatableTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the Superdesk Web Publisher Common Component.
+ *
+ * Copyright 2016 Sourcefabric z.u. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2016 Sourcefabric z.ú.
+ * @license http://www.superdesk.org/license
+ */
+namespace SWP\Component\Common\Model;
+
+trait TranslatableTrait
+{
+    /**
+     * @var string
+     */
+    protected $locale;
+
+    /**
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+}
