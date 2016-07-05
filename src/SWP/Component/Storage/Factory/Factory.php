07d0c68378535bb638dca75fc1d78616b86f5f17
Rafał Muszyński <rmuszynski1@gmail.com>
2016-07-05 18:04:41 +0200

    push more specs

--- src/SWP/Component/Storage/Factory/Factory.php
+++ src/SWP/Component/Storage/Factory/Factory.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the Superdesk Web Publisher Storage Component.
+ *
+ * Copyright 2016 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2016 Sourcefabric z.ú.
+ * @license http://www.superdesk.org/license
+ */
 namespace SWP\Component\Storage\Factory;
 
 class Factory implements FactoryInterface
