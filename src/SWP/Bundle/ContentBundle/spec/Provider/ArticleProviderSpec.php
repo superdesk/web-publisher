560efc1f9ce7e1e03e38ace8693766f93b348ea0
Rafał Muszyński <rmuszynski1@gmail.com>
2016-07-08 13:58:45 +0200

    removed empty line

--- src/SWP/Bundle/ContentBundle/spec/Provider/ArticleProviderSpec.php
+++ src/SWP/Bundle/ContentBundle/spec/Provider/ArticleProviderSpec.php
@@ -73,7 +73,7 @@ class ArticleProviderSpec extends ObjectBehavior
 
         $this->getOneById('111')->shouldBeNull();
     }
-    
+
     function it_gets_parent(
         TenantAwarePathBuilderInterface $pathBuilder,
         ArticleInterface $article,
