831a166eabac1ea1a5084ba22f0edab54b879326
Rafał Muszyński <rmuszynski1@gmail.com>
2016-08-10 17:16:41 +0200

    [SWP-242] - Rename WebRendererBundle to CoreBundle

---
+++ src/SWP/Bundle/CoreBundle/spec/Enhancer/RouteEnhancerSpec.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace spec\SWP\Bundle\CoreBundle\Enhancer;
+
+use PhpSpec\ObjectBehavior;
+use Symfony\Cmf\Component\Routing\Enhancer\RouteEnhancerInterface;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+use SWP\Bundle\CoreBundle\Resolver\TemplateNameResolver;
+use SWP\Bundle\ContentBundle\Model\RouteInterface;
+use SWP\Bundle\ContentBundle\Model\Article;
+use SWP\Component\TemplatesSystem\Gimme\Loader\LoaderInterface;
+
+class RouteEnhancerSpec extends ObjectBehavior
+{
+    public function let(LoaderInterface $metaLoader)
+    {
+        $this->beConstructedWith(new TemplateNameResolver(), $metaLoader);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('SWP\Bundle\CoreBundle\Enhancer\RouteEnhancer');
+    }
+
+    public function it_should_implement_interface()
+    {
+        $this->shouldImplement(RouteEnhancerInterface::class);
+    }
+
+    public function it_should_set_template_name(RouteInterface $route, Article $article)
+    {
+        $this->setTemplateName(null, [])->shouldReturn([
+            RouteObjectInterface::TEMPLATE_NAME => 'article.html.twig',
+        ]);
+
+        $this->setTemplateName(null, [RouteObjectInterface::ROUTE_OBJECT => $route])->shouldReturn([
+            RouteObjectInterface::ROUTE_OBJECT => $route,
+            RouteObjectInterface::TEMPLATE_NAME => 'article.html.twig',
+        ]);
+
+        $route->getTemplateName()->willReturn('test.html.twig');
+        $this->setTemplateName(null, [RouteObjectInterface::ROUTE_OBJECT => $route])->shouldReturn([
+            RouteObjectInterface::ROUTE_OBJECT => $route,
+            RouteObjectInterface::TEMPLATE_NAME => 'test.html.twig',
+        ]);
+
+
+        $route->getTemplateName()->willReturn('test.html.twig');
+        $article->getRoute()->willReturn($route);
+        $article->getTemplateName()->willReturn(null);
+        $this->setTemplateName($article, [RouteObjectInterface::ROUTE_OBJECT => $route])->shouldReturn([
+            RouteObjectInterface::ROUTE_OBJECT => $route,
+            RouteObjectInterface::TEMPLATE_NAME => 'test.html.twig',
+        ]);
+
+        $route->getTemplateName()->willReturn('test.html.twig');
+        $article->getRoute()->willReturn($route);
+        $article->getTemplateName()->willReturn('article2.html.twig');
+        $this->setTemplateName($article, [])->shouldReturn([
+            RouteObjectInterface::TEMPLATE_NAME => 'article2.html.twig',
+        ]);
+    }
+}
---
+++ src/SWP/Bundle/CoreBundle/spec/Theme/Factory/ThemeFactorySpec.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace spec\SWP\Bundle\CoreBundle\Theme\Factory;
+
+use PhpSpec\ObjectBehavior;
+use SWP\Bundle\CoreBundle\Theme\Factory\ThemeFactory;
+use Sylius\Bundle\ThemeBundle\Factory\ThemeFactoryInterface;
+use Sylius\Bundle\ThemeBundle\Model\ThemeInterface;
+
+class ThemeFactorySpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(ThemeFactory::class);
+    }
+
+    public function it_implements_theme_factory_interface()
+    {
+        $this->shouldImplement(ThemeFactoryInterface::class);
+    }
+
+    public function it_creates_a_theme()
+    {
+        $this->create('example/theme@subdomain', '/theme/path')->shouldHaveNameAndPath('example/theme', '/theme/path');
+        $this->create('example/theme', '/theme/path')->shouldHaveNameAndPath('example/theme', '/theme/path');
+    }
+
+    public function it_cant_create_a_theme()
+    {
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('create', ['testtheme', '/theme/path']);
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'haveNameAndPath' => function (ThemeInterface $theme, $expectedName, $expectedPath) {
+                return $expectedName === $theme->getName()
+                && $expectedPath === $theme->getPath();
+            },
+        ];
+    }
+}
