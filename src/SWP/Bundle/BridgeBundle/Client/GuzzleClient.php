b22c1445d8133d99ad8955708e958f529574ab6b
Rafał Muszyński <rmuszynski1@gmail.com>
2016-08-18 11:24:10 +0200

    removed use

--- src/SWP/Bundle/BridgeBundle/Client/GuzzleClient.php
+++ src/SWP/Bundle/BridgeBundle/Client/GuzzleClient.php
@@ -16,7 +16,6 @@ namespace SWP\Bundle\BridgeBundle\Client;
 use GuzzleHttp\Client as BaseClient;
 use GuzzleHttp\Exception\ClientException as GuzzleClientException;
 use GuzzleHttp\Exception\ServerException as GuzzleServerException;
-use GuzzleHttp\Exception\TransferException as GuzzleTransferException;
 use Superdesk\ContentApiSdk\Client\ClientInterface;
 use Superdesk\ContentApiSdk\Exception\ClientException;
 
