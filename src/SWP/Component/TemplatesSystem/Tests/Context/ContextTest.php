3c3b244d6e4259394085a56af0f90748182da12b
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2016-08-24 19:44:32 +0100

    still wip - commits history will be rebased

--- src/SWP/Component/TemplatesSystem/Tests/Context/ContextTest.php
+++ src/SWP/Component/TemplatesSystem/Tests/Context/ContextTest.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: pawelmikolajczuk
- * Date: 24.08.2016
- * Time: 16:56
+ * This file is part of the Superdesk Web Publisher Templates System.
+ *
+ * Copyright 2015 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2015 Sourcefabric z.ú.
+ * @license http://www.superdesk.org/license
  */
 
 namespace SWP\Component\TemplatesSystem\Tests\Context;
