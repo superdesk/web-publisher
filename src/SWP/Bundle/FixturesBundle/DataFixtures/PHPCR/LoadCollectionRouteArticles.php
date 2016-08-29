a1eff5c8f778aab350400929ab44f3d939e1b3e0
Rafał Muszyński <rmuszynski1@gmail.com>
2016-08-29 18:14:06 +0200

    code style fix

--- src/SWP/Bundle/FixturesBundle/DataFixtures/PHPCR/LoadCollectionRouteArticles.php
+++ src/SWP/Bundle/FixturesBundle/DataFixtures/PHPCR/LoadCollectionRouteArticles.php
@@ -14,7 +14,6 @@
 namespace SWP\Bundle\FixturesBundle\DataFixtures\PHPCR;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use SWP\Bundle\ContentBundle\Doctrine\ODM\PHPCR\Article;
 use SWP\Bundle\FixturesBundle\AbstractFixture;
