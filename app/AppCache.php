2fe69a6af7045e4c6e1240964d28bde26a30acce
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2016-09-23 13:10:42 +0100

    [config] controll better HttpCache sybscribers, add namespace to cache keys, disable symfony proxy in production

--- app/AppCache.php
+++ app/AppCache.php
@@ -3,18 +3,22 @@
 use FOS\HttpCache\SymfonyCache\PurgeSubscriber;
 use FOS\HttpCache\SymfonyCache\RefreshSubscriber;
 use FOS\HttpCacheBundle\SymfonyCache\EventDispatchingHttpCache;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class AppCache extends EventDispatchingHttpCache
 {
-    /**
-     * Overwrite constructor to register event subscribers for FOSHttpCache.
-     */
-    public function __construct(HttpKernelInterface $kernel, $cacheDir = null)
+    public function getOptions()
     {
-        parent::__construct($kernel, $cacheDir);
+        return array(
+            'fos_default_subscribers' => self::SUBSCRIBER_NONE,
+        );
+    }
 
-        $this->addSubscriber(new PurgeSubscriber());
-        $this->addSubscriber(new RefreshSubscriber());
+    public function getDefaultSubscribers()
+    {
+        $subscribers = parent::getDefaultSubscribers();
+        $subscribers[] = new PurgeSubscriber();
+        $subscribers[] = new RefreshSubscriber();
+
+        return $subscribers;
     }
 }
