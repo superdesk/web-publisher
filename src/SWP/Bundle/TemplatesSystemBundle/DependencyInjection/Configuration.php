e023de7cbe06869f51404313788b5cbef36f2692
Rafał Muszyński <rmuszynski1@gmail.com>
2017-01-26 10:34:40 +0100

    Fixed container service to be tenant aware (#227)
    * fixes
    
    * use proper repository

--- src/SWP/Bundle/TemplatesSystemBundle/DependencyInjection/Configuration.php
+++ src/SWP/Bundle/TemplatesSystemBundle/DependencyInjection/Configuration.php
@@ -23,6 +23,7 @@ use SWP\Bundle\TemplatesSystemBundle\Model\ContainerData;
 use SWP\Bundle\TemplatesSystemBundle\Model\ContainerWidget;
 use SWP\Bundle\TemplatesSystemBundle\Model\WidgetModel;
 use SWP\Bundle\TemplatesSystemBundle\Repository\ContainerRepository;
+use SWP\Bundle\TemplatesSystemBundle\Repository\ContainerWidgetRepository;
 use SWP\Bundle\TemplatesSystemBundle\Repository\WidgetModelRepository;
 use SWP\Component\Storage\Factory\Factory;
 use SWP\Component\TemplatesSystem\Gimme\Model\ContainerDataInterface;
@@ -92,8 +93,8 @@ class Configuration implements ConfigurationInterface
                                             ->children()
                                                 ->scalarNode('model')->cannotBeEmpty()->defaultValue(ContainerWidget::class)->end()
                                                 ->scalarNode('interface')->cannotBeEmpty()->defaultValue(ContainerWidgetInterface::class)->end()
-                                                ->scalarNode('repository')->defaultValue(null)->end()
-                                                ->scalarNode('factory')->defaultValue(null)->end()
+                                                ->scalarNode('repository')->defaultValue(ContainerWidgetRepository::class)->end()
+                                                ->scalarNode('factory')->defaultValue(Factory::class)->end()
                                                 ->scalarNode('object_manager_name')->defaultValue(null)->end()
                                             ->end()
                                         ->end()
