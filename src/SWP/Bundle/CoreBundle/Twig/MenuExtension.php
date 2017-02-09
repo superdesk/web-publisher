3291799976cd84ef56e9421c22c889153b45957d
Rafał Muszyński <rmuszynski1@gmail.com>
2017-02-09 16:58:42 +0100

    copyright block

--- src/SWP/Bundle/CoreBundle/Twig/MenuExtension.php
+++ src/SWP/Bundle/CoreBundle/Twig/MenuExtension.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Superdesk Web Publisher Core Bundle.
+ *
+ * Copyright 2017 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2017 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
 declare(strict_types=1);
 
 namespace SWP\Bundle\CoreBundle\Twig;
