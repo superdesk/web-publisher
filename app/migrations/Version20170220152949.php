855c9ed245682b3438457061c23974f7fe71be86
Rafał Muszyński <rmuszynski1@gmail.com>
2017-02-20 20:30:11 +0100

    byline migration file (#264)

---
+++ app/migrations/Version20170220152949.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170220152949 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_item ALTER byline DROP NOT NULL');
+        $this->addSql('ALTER TABLE swp_package ALTER byline DROP NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_item ALTER byline SET NOT NULL');
+        $this->addSql('ALTER TABLE swp_package ALTER byline SET NOT NULL');
+    }
+}
