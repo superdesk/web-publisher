bdf5fd4a8a27b4ddf81d3f35b9cf0b889de50362
Rafał Muszyński <rmuszynski1@gmail.com>
2017-03-03 17:02:09 +0100

    cs fix

--- src/SWP/Bundle/FixturesBundle/DataFixtures/ORM/LoadAmpHtmlData.php
+++ src/SWP/Bundle/FixturesBundle/DataFixtures/ORM/LoadAmpHtmlData.php
@@ -4,7 +4,6 @@ namespace SWP\Bundle\FixturesBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use SWP\Bundle\ContentBundle\Model\ArticleInterface;
 use SWP\Bundle\FixturesBundle\AbstractFixture;
 
 class LoadAmpHtmlData extends AbstractFixture implements FixtureInterface
