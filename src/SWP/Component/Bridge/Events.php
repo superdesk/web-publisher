3eb17592ad473e986cd5bf98dc98bf884d9186a4
Rafał Muszyński <rmuszynski1@gmail.com>
2017-03-24 12:56:03 +0100

    added mising class

---
+++ src/SWP/Component/Bridge/Events.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Superdesk Web Publisher Bridge Component.
+ *
+ * Copyright 2017 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2017 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
+namespace SWP\Component\Bridge;
+
+final class Events
+{
+    /**
+     * The SWP_VALIDATION event occurs on an object validation.
+     *
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     *
+     * @var string
+     */
+    const SWP_VALIDATION = 'swp.validation';
+
+    private function __construct()
+    {
+    }
+}
