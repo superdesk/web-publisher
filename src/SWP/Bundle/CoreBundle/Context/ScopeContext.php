31161b1ca19c93c1962465d48cb185d22d671a1e
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2017-04-04 13:18:19 +0200

    remove coverage check from scrutinizer

--- src/SWP/Bundle/CoreBundle/Context/ScopeContext.php
+++ src/SWP/Bundle/CoreBundle/Context/ScopeContext.php
@@ -3,12 +3,12 @@
 /*
  * This file is part of the Superdesk Web Publisher Core Bundle.
  *
- * Copyright 2016 Sourcefabric z.u. and contributors.
+ * Copyright 2017 Sourcefabric z.u. and contributors.
  *
  * For the full copyright and license information, please see the
  * AUTHORS and LICENSE files distributed with this source code.
  *
- * @copyright 2016 Sourcefabric z.ú
+ * @copyright 2017 Sourcefabric z.ú
  * @license http://www.superdesk.org/license
  */
 
