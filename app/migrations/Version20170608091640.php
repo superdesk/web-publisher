061ca364cb49295cefee5b0df4dac1910fdbf54a
Rafał Muszyński <rmuszynski1@gmail.com>
2017-06-08 12:57:29 +0200

    add migrations

---
+++ app/migrations/Version20170608091640.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170608091640 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_rule ADD description VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE swp_rule ADD name VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_rule DROP description');
+        $this->addSql('ALTER TABLE swp_rule DROP name');
+    }
+}
