51d3f5cefd6675de3d42e00623d3f8d8ac2d2a25
Rafał Muszyński <rmuszynski1@gmail.com>
2017-06-09 12:52:57 +0200

    fixed usage_terms limit

---
+++ app/migrations/Version20170609104710.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170609104710 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_article_media ALTER usage_terms TYPE TEXT');
+        $this->addSql('UPDATE swp_article SET package_id = (SELECT p.id FROM swp_package AS p WHERE p.headline = title ORDER BY p.id DESC LIMIT 1');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_article_media ALTER usage_terms TYPE VARCHAR(255)');
+    }
+}
