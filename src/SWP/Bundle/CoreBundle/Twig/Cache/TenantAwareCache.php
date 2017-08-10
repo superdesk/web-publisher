8e864c73bcdfd193962da7464faf7b61f5f11c93
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2017-08-10 11:45:15 +0200

    add tests

--- src/SWP/Bundle/CoreBundle/Twig/Cache/TenantAwareCache.php
+++ src/SWP/Bundle/CoreBundle/Twig/Cache/TenantAwareCache.php
@@ -3,44 +3,43 @@
 /*
  * This file is part of the Superdesk Web Publisher Core Bundle.
  *
- * Copyright 2016 Sourcefabric z.u. and contributors.
+ * Copyright 2017 Sourcefabric z.u. and contributors.
  *
  * For the full copyright and license information, please see the
  * AUTHORS and LICENSE files distributed with this source code.
  *
- * @copyright 2016 Sourcefabric z.ú
+ * @copyright 2017 Sourcefabric z.ú
  * @license http://www.superdesk.org/license
  */
 
 namespace SWP\Bundle\CoreBundle\Twig\Cache;
 
 use SWP\Bundle\MultiTenancyBundle\Context\TenantContext;
-use Sylius\Bundle\ThemeBundle\Context\ThemeContextInterface;
 
 /**
- * Implements a cache on the filesystem.
- *
- * @author Andrew Tch <andrew@noop.lv>
+ * Implements tenant aware Twig cache.
  */
 class TenantAwareCache extends \Twig_Cache_Filesystem
 {
-    const FORCE_BYTECODE_INVALIDATION = 1;
-
+    /**
+     * @var string
+     */
     private $directory;
-    private $themeContext;
+
+    /**
+     * @var TenantContext
+     */
     private $tenantContext;
 
     /**
      * TenantAwareCache constructor.
      *
-     * @param string                $directory
-     * @param ThemeContextInterface $themeContext
-     * @param TenantContext         $tenantContext
+     * @param string        $directory
+     * @param TenantContext $tenantContext
      */
-    public function __construct(string $directory, ThemeContextInterface $themeContext, TenantContext $tenantContext)
+    public function __construct(string $directory, TenantContext $tenantContext)
     {
         $this->directory = $directory;
-        $this->themeContext = $themeContext;
         $this->tenantContext = $tenantContext;
 
         parent::__construct($this->directory);
