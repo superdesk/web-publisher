84a6a2813a269ba5f52033a69bc73e515f0a996d
Rafał Muszyński <rmuszynski1@gmail.com>
2017-08-18 12:09:12 +0200

    migrations fix

--- app/migrations/Version20170818100707.php
+++ app/migrations/Version20170818100707.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170725125727 extends AbstractMigration
+class Version20170818100707 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,10 +18,10 @@ class Version20170725125727 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE swp_article_source (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE swp_article_sources (article_id INT NOT NULL, source_id INT NOT NULL, PRIMARY KEY(article_id, source_id))');
         $this->addSql('CREATE INDEX IDX_E38D33537294869C ON swp_article_sources (article_id)');
         $this->addSql('CREATE INDEX IDX_E38D3353953C1C61 ON swp_article_sources (source_id)');
+        $this->addSql('CREATE TABLE swp_article_source (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, tenant_code VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('ALTER TABLE swp_article_sources ADD CONSTRAINT FK_E38D33537294869C FOREIGN KEY (article_id) REFERENCES swp_article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE swp_article_sources ADD CONSTRAINT FK_E38D3353953C1C61 FOREIGN KEY (source_id) REFERENCES swp_article_source (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE swp_article DROP source');
@@ -36,8 +36,8 @@ class Version20170725125727 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE swp_article_sources DROP CONSTRAINT FK_E38D3353953C1C61');
-        $this->addSql('DROP TABLE swp_article_source');
         $this->addSql('DROP TABLE swp_article_sources');
+        $this->addSql('DROP TABLE swp_article_source');
         $this->addSql('ALTER TABLE swp_article ADD source VARCHAR(255) DEFAULT NULL');
     }
 }
