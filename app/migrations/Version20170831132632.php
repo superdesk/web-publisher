fc16d3843f7fd42b7bce1f129fc46a2be04e5f45
Rafał Muszyński <rmuszynski1@gmail.com>
2017-08-31 15:32:31 +0200

    fix index for tenant table

---
+++ app/migrations/Version20170831132632.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170831132632 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX host_idx');
+        $this->addSql('CREATE UNIQUE INDEX host_idx ON swp_tenant (domain_name, subdomain, deleted_at)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX host_idx');
+        $this->addSql('CREATE UNIQUE INDEX host_idx ON swp_tenant (domain_name, subdomain)');
+    }
+}
