92818b7f64a4158a58bd0d5bc11acc10489a7404
Rafał Muszyński <rmuszynski1@gmail.com>
2017-09-22 18:08:37 +0200

    append liveditor scripts when activated from API (#36)
    * test fix, set theme bundle version
    
    * update jms serializer
    
    * change serializer bridge version

--- src/SWP/Bundle/CoreBundle/Tests/Controller/AuthControllerTest.php
+++ src/SWP/Bundle/CoreBundle/Tests/Controller/AuthControllerTest.php
@@ -261,8 +261,9 @@ class AuthControllerTest extends WebTestCase
         $client->request('GET', $content['url']);
         self::assertEquals(302, $client->getResponse()->getStatusCode());
         $cookies = $client->getResponse()->headers->getCookies();
-        self::assertCount(1, $cookies);
+        self::assertCount(2, $cookies);
         self::assertEquals('activate_livesite_editor', $cookies[0]->getName());
+        self::assertEquals('append_livesite_editor_scripts', $cookies[1]->getName());
         $client->followRedirect();
         self::assertEquals(200, $client->getResponse()->getStatusCode());
 
