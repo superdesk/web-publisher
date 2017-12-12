5c06395d115d9400e0bfe650b7e40c2000b6f90c
Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
2017-12-12 12:35:40 +0100

    Apply fixes from StyleCI (#38)

--- app/migrations/Version20171202141557.php
+++ app/migrations/Version20171202141557.php
@@ -16,7 +16,7 @@ class Version20171202141557 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE swp_webhook ADD tenant_code VARCHAR(255) DEFAULT NULL');
     }
@@ -27,7 +27,7 @@ class Version20171202141557 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE swp_webhook DROP tenant_code');
     }
