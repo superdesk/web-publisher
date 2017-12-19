c05e1d4fb9209a92bfa6d03c59104bbc1b552827
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2017-12-19 10:07:50 +0100

    add article statistics autmatic creation on publishing

--- src/SWP/Bundle/CoreBundle/Service/ArticlePublisher.php
+++ src/SWP/Bundle/CoreBundle/Service/ArticlePublisher.php
@@ -21,12 +21,14 @@ use SWP\Bundle\ContentBundle\Doctrine\ArticleRepositoryInterface;
 use SWP\Bundle\ContentBundle\Event\ArticleEvent;
 use SWP\Bundle\ContentBundle\Factory\ArticleFactoryInterface;
 use SWP\Bundle\CoreBundle\Model\ArticleInterface;
+use SWP\Bundle\CoreBundle\Model\ArticleStatisticsInterface;
 use SWP\Bundle\CoreBundle\Model\CompositePublishActionInterface;
 use SWP\Bundle\CoreBundle\Model\PackageInterface;
 use SWP\Bundle\CoreBundle\Model\PublishDestinationInterface;
 use SWP\Bundle\CoreBundle\Model\TenantInterface;
 use SWP\Component\Bridge\Events;
 use SWP\Component\MultiTenancy\Context\TenantContextInterface;
+use SWP\Component\Storage\Factory\FactoryInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
@@ -48,6 +50,11 @@ final class ArticlePublisher implements ArticlePublisherInterface
     private $articleFactory;
 
     /**
+     * @var FactoryInterface
+     */
+    private $articleStatisticsFactory;
+
+    /**
      * @var TenantContextInterface
      */
     private $tenantContext;
@@ -58,13 +65,15 @@ final class ArticlePublisher implements ArticlePublisherInterface
      * @param ArticleRepositoryInterface $articleRepository
      * @param EventDispatcherInterface   $eventDispatcher
      * @param ArticleFactoryInterface    $articleFactory
+     * @param FactoryInterface           $articleStatisticsFactory
      * @param TenantContextInterface     $tenantContext
      */
-    public function __construct(ArticleRepositoryInterface $articleRepository, EventDispatcherInterface $eventDispatcher, ArticleFactoryInterface $articleFactory, TenantContextInterface $tenantContext)
+    public function __construct(ArticleRepositoryInterface $articleRepository, EventDispatcherInterface $eventDispatcher, ArticleFactoryInterface $articleFactory, FactoryInterface $articleStatisticsFactory, TenantContextInterface $tenantContext)
     {
         $this->articleRepository = $articleRepository;
         $this->eventDispatcher = $eventDispatcher;
         $this->articleFactory = $articleFactory;
+        $this->articleStatisticsFactory = $articleStatisticsFactory;
         $this->tenantContext = $tenantContext;
     }
 
@@ -107,10 +116,15 @@ final class ArticlePublisher implements ArticlePublisherInterface
 
             /** @var ArticleInterface $article */
             $article = $this->articleFactory->createFromPackage($package);
+            /** @var ArticleStatisticsInterface $articleStatistics */
+            $articleStatistics = $this->articleStatisticsFactory->create();
+            $articleStatistics->setArticle($article);
+            $this->articleRepository->persist($articleStatistics);
             $this->eventDispatcher->dispatch(Events::SWP_VALIDATION, new GenericEvent($article));
             $article->setPackage($package);
             $article->setRoute($destination->getRoute());
             $article->setPublishedFBIA($destination->isFbia());
+            $article->setArticleStatistics($articleStatistics);
             $this->articleRepository->persist($article);
             $this->dispatchEvents($article, $package);
         }
