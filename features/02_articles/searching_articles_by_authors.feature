45aa1c8b5f474867eaa0d2ffbf9f7445a6dc20d4
Rafał Muszyński <rmuszynski1@gmail.com>
2018-01-30 13:13:38 +0100

    fix searching articles by author (#433)

---
+++ features/02_articles/searching_articles_by_authors.feature
@@ -0,0 +1,14 @@
+@articles
+Feature: Filtering/searching existing articles by authors
+  In order to pfilter articles by given author
+  As a HTTP Client
+  I want to be able to check if filtering works properly
+
+  Scenario: Searching/filtering articles by authors
+    And I am authenticated as "test.user"
+    And I add "Content-Type" header equal to "application/json"
+    And I run "fos:elastica:populate --env=test" command
+    Then I should see "Refreshing swp" in the output
+    Then I send a "GET" request to "/api/{version}/content/articles/?author[]=Tom"
+    Then the response status code should be 200
+    And the JSON node "total" should be equal to "1"
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -8,4 +8,23 @@ use Behat\Behat\Context\SnippetAcceptingContext;
  */
 class FeatureContext implements Context, SnippetAcceptingContext
 {
+    private $output = '';
+
+    /**
+     * @When I run :command command
+     */
+    public function iRunCommand($command)
+    {
+        $this->output = shell_exec(sprintf('php app/console %s', $command));
+    }
+
+    /**
+     * @Then I should see :result in the output
+     */
+    public function iShouldSeeInTheOutput(string $result)
+    {
+        if (false === strpos($this->output, $result)) {
+            throw new \Exception(sprintf('Could not see "%s" in output "%s"', $result, $this->output));
+        }
+    }
 }
