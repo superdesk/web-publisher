1aa357f92417c7d28fc5a3a8b3aca0e5a85ffe01
Rafał Muszyński <rmuszynski1@gmail.com>
2018-03-20 13:34:59 +0100

    create a place for behat classes

--- src/SWP/Behat/Service/RandomStringGeneratorStub.php
+++ src/SWP/Behat/Service/RandomStringGeneratorStub.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+namespace SWP\Behat\Service;
+
 use SWP\Component\Common\Generator\GeneratorInterface;
 
 final class RandomStringGeneratorStub implements GeneratorInterface
