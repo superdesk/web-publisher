7a6d78c30524db3e8c49c7e5a6ee8136cff81ada
Rafał Muszyński <rmuszynski1@gmail.com>
2018-03-20 15:36:10 +0100

    fixed extra setters/getters

--- src/SWP/Bundle/ContentBundle/Model/Article.php
+++ src/SWP/Bundle/ContentBundle/Model/Article.php
@@ -485,7 +485,7 @@ class Article implements ArticleInterface
     /**
      * {@inheritdoc}
      */
-    public function getExtra(): array
+    public function getExtra(): ?array
     {
         return $this->extra;
     }
@@ -493,7 +493,7 @@ class Article implements ArticleInterface
     /**
      * {@inheritdoc}
      */
-    public function setExtra(array $extra): void
+    public function setExtra(?array $extra): void
     {
         $this->extra = $extra;
     }
