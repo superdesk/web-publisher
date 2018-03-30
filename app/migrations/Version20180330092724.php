83e509cc4e8a1f6009fbcbf6d1edf552477fe1fd
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2018-03-30 11:24:03 +0200

    add missing ednote to package in db

---
+++ app/migrations/Version20180330092724.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180330092724 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_package ADD ed_note TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE swp_package ADD genre TEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_package DROP ed_note');
+        $this->addSql('ALTER TABLE swp_package DROP genre');
+    }
+}
