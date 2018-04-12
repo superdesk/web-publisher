83a62d94aa95f57edca0812409fc809fab245002
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2018-04-12 13:46:51 +0200

    check if route exists by name, improve test

--- src/SWP/Bundle/CoreBundle/Theme/Generator/ThemeRoutesGenerator.php
+++ src/SWP/Bundle/CoreBundle/Theme/Generator/ThemeRoutesGenerator.php
@@ -86,7 +86,8 @@ class ThemeRoutesGenerator implements GeneratorInterface
             $cleanRouteData = $routeData;
             unset($cleanRouteData['numberOfArticles']);
             $route = $this->createRoute($cleanRouteData);
-            if ((null !== $existingRoute = $this->routeProvider->getOneByStaticPrefix($route->getStaticPrefix())) && ($existingRoute->getName() === $route->getName())) {
+            $existingRoute = $this->routeProvider->getRouteByName($route->getName());
+            if (null !== $existingRoute) {
                 continue;
             }
 
