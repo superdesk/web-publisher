c2155788bd29392a8d164a7977c59e8355abcc9d
Rafał Muszyński <rmuszynski1@gmail.com>
2018-04-13 11:23:46 +0200

    make output channel optional

--- src/SWP/Bundle/OutputChannelBundle/Form/Type/OutputChannelType.php
+++ src/SWP/Bundle/OutputChannelBundle/Form/Type/OutputChannelType.php
@@ -19,7 +19,7 @@ namespace SWP\Bundle\OutputChannelBundle\Form\Type;
 use SWP\Bundle\CoreBundle\Model\OutputChannel;
 use SWP\Component\OutputChannel\Model\OutputChannelInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -35,10 +35,10 @@ final class OutputChannelType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('type', TextType::class, [
-                'constraints' => [
-                    new NotBlank(),
-                ],
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Wordpress' => OutputChannelInterface::TYPE_WORDPRESS,
+                ]
             ])
         ;
 
