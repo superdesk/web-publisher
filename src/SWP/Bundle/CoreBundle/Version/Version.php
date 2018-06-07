624aefb22c26a4548dd8c7dc7e05eabf97a968a4
Rafał Muszyński <rmuszynski1@gmail.com>
2018-06-07 15:21:21 +0200

    [release] Released 1.4.0-beta version

--- src/SWP/Bundle/CoreBundle/Version/Version.php
+++ src/SWP/Bundle/CoreBundle/Version/Version.php
@@ -24,7 +24,7 @@ final class Version
      *
      * @var string
      */
-    protected $version = '1.3.0';
+    protected $version = '1.4.0-beta';
 
     /**
      * Code name string.
@@ -38,7 +38,7 @@ final class Version
      *
      * @var string
      */
-    protected $releaseDate = '2018-04-05';
+    protected $releaseDate = '2018-06-07';
 
     /**
      * @return string
