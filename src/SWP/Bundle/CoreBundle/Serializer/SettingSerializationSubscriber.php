ce3aaedff53ff586d13686ded4c2e73e99e4ea63
Rafał Muszyński <rmuszynski1@gmail.com>
2018-06-20 16:22:49 +0200

    added setting serialization subscriber

---
+++ src/SWP/Bundle/CoreBundle/Serializer/SettingSerializationSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Superdesk Web Publisher Core Bundle.
+ *
+ * Copyright 2018 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2018 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
+namespace SWP\Bundle\CoreBundle\Serializer;
+
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use SWP\Bundle\CoreBundle\Model\Settings;
+
+final class SettingSerializationSubscriber implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            [
+                'event' => 'serializer.post_serialize',
+                'class' => Settings::class,
+                'method' => 'onPostSerialize',
+            ],
+        ];
+    }
+
+    /**
+     * @param ObjectEvent $event
+     */
+    public function onPostSerialize(ObjectEvent $event)
+    {
+        $data = $event->getObject();
+
+        if (\is_bool($data->getValue())) {
+            $event->getVisitor()->setData('value', (bool) $data->getValue());
+        }
+    }
+}
