af9d57ae3a7e0642fdb25f026d08fde16f7e4d4b
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2018-06-29 16:09:45 +0200

    remove unused code and trashes

--- src/SWP/Bundle/FixturesBundle/WebTestCase.php
+++ src/SWP/Bundle/FixturesBundle/WebTestCase.php
@@ -57,15 +57,6 @@ class WebTestCase extends BaseWebTestCase
         }
     }
 
-    protected function getCustomFixtures(array $fixtures)
-    {
-        $env = $this->getContainer()->getParameter('test_env');
-        $registry = new FixtureRegistry();
-        $registry->setEnvironment($env);
-
-        return $registry->getFixtures($fixtures);
-    }
-
     protected function loadCustomFixtures(array $fixtures)
     {
         $env = $this->getContainer()->getParameter('test_env');
