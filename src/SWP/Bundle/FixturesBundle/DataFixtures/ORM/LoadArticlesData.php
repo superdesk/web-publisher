b8b46cb5686cb9bea368e70d481e5f89ba761e99
Rafał Muszyński <rmuszynski1@gmail.com>
2018-08-07 14:07:13 +0200

    cast to int

--- src/SWP/Bundle/FixturesBundle/DataFixtures/ORM/LoadArticlesData.php
+++ src/SWP/Bundle/FixturesBundle/DataFixtures/ORM/LoadArticlesData.php
@@ -590,8 +590,8 @@ class LoadArticlesData extends AbstractFixture implements FixtureInterface, Depe
 
         imagecopyresampled($newImage,
             $image,
-            0 - (int) ($newImageWidth - $renditionWidth) / 2,
-            0 - (int) ($newImageHeight - $renditionHeight) / 2,
+            0 - (int) (($newImageWidth - $renditionWidth) / 2),
+            0 - (int) (($newImageHeight - $renditionHeight) / 2),
             0,
             0,
             (int) $newImageWidth,
