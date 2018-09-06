5a13d0745116396011a76beeb2ef428fdfed32a7
Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
2018-09-06 12:19:30 +0200

    Apply fixes from StyleCI (#47)

--- src/SWP/Bundle/AnalyticsBundle/Services/ArticleStatisticsServiceInterface.php
+++ src/SWP/Bundle/AnalyticsBundle/Services/ArticleStatisticsServiceInterface.php
@@ -11,7 +11,9 @@ interface ArticleStatisticsServiceInterface
     const KEY_PAGEVIEW_SOURCE = 'pageViewSource';
 
     const KEY_IMPRESSION_SOURCE_ARTICLE = 'sourceArticle';
+
     const KEY_IMPRESSION_SOURCE_ROUTE = 'sourceRoute';
+
     const KEY_IMPRESSION_TYPE = 'type';
 
     public function addArticleEvent(int $articleId, string $action, array $extraData): ArticleStatisticsInterface;
