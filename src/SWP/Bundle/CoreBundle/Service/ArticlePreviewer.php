a434ed56f380451d34a26a775c4335d875057c89
Rafał Muszyński <rmuszynski1@gmail.com>
2018-11-06 13:28:30 +0100

    revert article previewer changes

--- src/SWP/Bundle/CoreBundle/Service/ArticlePreviewer.php
+++ src/SWP/Bundle/CoreBundle/Service/ArticlePreviewer.php
@@ -42,11 +42,6 @@ final class ArticlePreviewer implements ArticlePreviewerInterface
     private $articlePreviewHelper;
 
     /**
-     * @var bool
-     */
-    private $isPreview = false;
-
-    /**
      * ArticlePreviewer constructor.
      *
      * @param ArticleFactoryInterface               $articleFactory
@@ -68,8 +63,6 @@ final class ArticlePreviewer implements ArticlePreviewerInterface
      */
     public function preview(PackageInterface $package, RouteInterface $route): ArticleInterface
     {
-        $this->isPreview = true;
-
         $article = $this->articleFactory->createFromPackage($package);
         $this->articleMediaProcessor->fillArticleMedia($package, $article);
         ArticleAuthorProcessor::processArticleAuthors($article);
@@ -82,10 +75,5 @@ final class ArticlePreviewer implements ArticlePreviewerInterface
         $this->articlePreviewHelper->enablePreview($article);
 
         return $article;
-    }
-
-    public function isPreview(): bool
-    {
-        return $this->isPreview;
     }
 }
