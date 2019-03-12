eb78e39deff6382515864a10247200e9bc42c7fd
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2019-03-12 17:06:10 +0100

    remove comments from code

--- features/bootstrap/config/services.php
+++ features/bootstrap/config/services.php
@@ -2,7 +2,5 @@
 
 declare(strict_types=1);
 
-// features/bootstrap/config/services.php
-
 $container->setParameter('behat.doctrine_data_fixtures.service.hook_listener.class', \SWP\Behat\Listener\FixturesHookListener::class);
 $container->setParameter('behat.doctrine_data_fixtures.service.fixture_loader.class', \SWP\Behat\Service\FixtureService::class);
