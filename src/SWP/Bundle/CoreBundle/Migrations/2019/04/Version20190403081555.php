98c451a6f3e2a78e33364dd69d74a247dc2092fc
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2019-04-03 10:50:02 +0200

    update migration, remove deleted article media

--- src/SWP/Bundle/CoreBundle/Migrations/2019/04/Version20190403081555.php
+++ src/SWP/Bundle/CoreBundle/Migrations/2019/04/Version20190403081555.php
@@ -17,16 +17,20 @@ final class Version20190403081555 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE swp_article_media DROP CONSTRAINT FK_B9721F7E7294869C');
+        $this->addSql('
+            UPDATE 
+                swp_article 
+            SET 
+                feature_media = NULL
+            WHERE 
+                  feature_media IN (
+                    SELECT id FROM swp_article_media WHERE deleted_at IS NOT NULL 
+                  )
+        ');
+        $this->addSql('DELETE FROM swp_article_media WHERE deleted_at IS NOT NULL');
         $this->addSql('ALTER TABLE swp_article_media DROP deleted_at');
         $this->addSql('ALTER TABLE swp_article_media ALTER updated_at TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
         $this->addSql('ALTER TABLE swp_article_media ALTER updated_at DROP DEFAULT');
-        $this->addSql('ALTER TABLE 
-          swp_article_media 
-        ADD 
-          CONSTRAINT FK_B9721F7E7294869C FOREIGN KEY (article_id) REFERENCES swp_article (id) ON DELETE 
-        SET 
-          NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
@@ -34,13 +38,8 @@ final class Version20190403081555 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE swp_article_media DROP CONSTRAINT fk_b9721f7e7294869c');
         $this->addSql('ALTER TABLE swp_article_media ADD deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
         $this->addSql('ALTER TABLE swp_article_media ALTER updated_at TYPE DATE');
         $this->addSql('ALTER TABLE swp_article_media ALTER updated_at DROP DEFAULT');
-        $this->addSql('ALTER TABLE 
-          swp_article_media 
-        ADD 
-          CONSTRAINT fk_b9721f7e7294869c FOREIGN KEY (article_id) REFERENCES swp_article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 }
