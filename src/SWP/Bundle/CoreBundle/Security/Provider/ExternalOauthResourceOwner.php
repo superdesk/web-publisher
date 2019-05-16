d1814a42382128ef8136ed9a1ed712cdc2df8ef4
Niklas Vanhainen <niklas.vanhainen@etc.se>
2019-05-16 18:28:31 +0200

    Partial implementation of Oauth provider and resource owner classes

---
+++ src/SWP/Bundle/CoreBundle/Security/Provider/ExternalOauthResourceOwner.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SWP\Bundle\CoreBundle\Security\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Tool\ArrayAccessorTrait;
+
+class ExternalOauthResourceOwner extends ResourceOwnerInterface
+{
+    use ArrayAccessorTrait;
+
+    protected $response;
+
+    public function __construct(array $response = [])
+    {
+        $this->response = $response;
+    }
+
+    public function getId()
+    {
+        return $this->getValueByKey($this->response, 'user_id');
+    }
+
+    public function getEmail()
+    {
+        return $this->getValueByKey($this->response, 'email');
+    }
+
+    public function getName()
+    {
+        return $this->getValueByKey($this->response, 'name');
+    }
+
+    public function toArray()
+    {
+        return $this->getValueByKey($this->response);
+    }
+}
