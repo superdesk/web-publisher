73b37d2b588c042090bfdb1ff78f8943adc4e036
Niklas Vanhainen <niklas.vanhainen@etc.se>
2019-06-04 10:36:09 +0200

    Added imports and changed name of class in ExternalOauthController

--- src/SWP/Bundle/CoreBundle/Controller/ExternalOauthController.php
+++ src/SWP/Bundle/CoreBundle/Controller/ExternalOauthController.php
@@ -1,8 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SWP\Bundle\CoreBundle\Controller;
 
-class OauthController extends Controller
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExternalOauthController extends Controller
 {
     /**
      * @Route("/connect/oauth", name="connect_oauth_start")
