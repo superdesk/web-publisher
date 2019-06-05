af3377e9568092ff13d9f54b7beb337f7424ccf3
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2019-06-05 11:32:48 +0200

    fix url generation for aws located media files (#859)
    * fix url generation for aws located media files
    * add spec for media manager logic
    * update deps
    * keep stable twig requirement in user bundle

--- src/SWP/Bundle/ContentBundle/spec/Manager/MediaManagerSpec.php
+++ src/SWP/Bundle/ContentBundle/spec/Manager/MediaManagerSpec.php
@@ -15,12 +15,16 @@
 namespace spec\SWP\Bundle\ContentBundle\Manager;
 
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use SWP\Bundle\ContentBundle\Doctrine\ArticleMediaRepositoryInterface;
 use SWP\Bundle\ContentBundle\Factory\FileFactoryInterface;
 use SWP\Bundle\ContentBundle\Manager\MediaManager;
 use League\Flysystem\Filesystem;
+use SWP\Bundle\ContentBundle\Model\FileInterface;
 use SWP\Bundle\ContentBundle\Resolver\AssetLocationResolverInterface;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouterInterface;
 
 class MediaManagerSpec extends ObjectBehavior
 {
@@ -30,12 +34,25 @@ class MediaManagerSpec extends ObjectBehavior
         Router $router,
         FileFactoryInterface $fileFactory,
         AssetLocationResolverInterface $assetLocationResolver
-    ) {
+    ): void {
+        $router->getContext()->willReturn(new RequestContext());
+        $assetLocationResolver->getAssetUrl(Argument::type(FileInterface::class))->willReturn(
+            'https://localhost.test/swp/media/file.png',
+            'swp/media/file.png',
+            'swp/media/file.png'
+        );
         $this->beConstructedWith($mediaRepository, $filesystem, $router, $fileFactory, $assetLocationResolver);
     }
 
-    public function it_is_initializable()
+    public function it_is_initializable(): void
     {
         $this->shouldHaveType(MediaManager::class);
+    }
+
+    public function it_get_correct_media_uri(FileInterface $file): void
+    {
+        $this->getMediaUri($file)->shouldReturn('https://localhost.test/swp/media/file.png');
+        $this->getMediaUri($file)->shouldReturn('/swp/media/file.png');
+        $this->getMediaUri($file, RouterInterface::ABSOLUTE_URL)->shouldReturn('http://localhost/swp/media/file.png');
     }
 }
