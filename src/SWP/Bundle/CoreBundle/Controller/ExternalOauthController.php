f8983552b46cbe7f1658fed58db7bdce72b336a6
Niklas Vanhainen <niklas.vanhainen@etc.se>
2019-06-13 16:23:50 +0200

    Clarify comment on connectCheckAction

--- src/SWP/Bundle/CoreBundle/Controller/ExternalOauthController.php
+++ src/SWP/Bundle/CoreBundle/Controller/ExternalOauthController.php
@@ -23,13 +23,12 @@ class ExternalOauthController extends Controller
         return $clientRegistry
             ->getClient('external_oauth')
             ->redirect([
-                'openid', 'email'
+                'openid', 'email', 'profile'
             ]);
     }
 
     /**
-     * This is the redirect route.
-     * This is where the user is redirected after being authenticated by the OAuth server.
+     * This is where the user is redirected after being succesfully authenticated by the OAuth server.
      * @Route("/connect/oauth/check", name="connect_oauth_check")
      */
     public function connectCheckAction(Request $request)
