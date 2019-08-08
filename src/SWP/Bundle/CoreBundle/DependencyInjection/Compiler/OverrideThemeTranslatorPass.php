a17b3c394b03a026d3e53f4abc2e53a4588894db
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2019-08-08 14:27:01 +0200

    load translations only from current tenant themes

--- src/SWP/Bundle/CoreBundle/DependencyInjection/Compiler/OverrideThemeTranslatorPass.php
+++ src/SWP/Bundle/CoreBundle/DependencyInjection/Compiler/OverrideThemeTranslatorPass.php
@@ -14,6 +14,7 @@
 
 namespace SWP\Bundle\CoreBundle\DependencyInjection\Compiler;
 
+use SWP\Bundle\CoreBundle\Theme\Translation\TenantAwareThemeTranslatorResourceProvider;
 use SWP\Bundle\CoreBundle\Theme\Translation\ThemeAwareTranslator;
 use SWP\Bundle\CoreBundle\Translation\MessageFormatter;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -36,5 +37,11 @@ final class OverrideThemeTranslatorPass extends AbstractOverridePass
             'translator.formatter.default',
             MessageFormatter::class
         );
+
+        $tenantAwareThemeTranslatorResourceProviderDefinition = $this->getDefinitionIfExists($container, 'sylius.theme.translation.resource_provider.theme_aware');
+        if (null !== $tenantAwareThemeTranslatorResourceProviderDefinition) {
+            $tenantAwareThemeTranslatorResourceProviderDefinition->setClass(TenantAwareThemeTranslatorResourceProvider::class);
+            $tenantAwareThemeTranslatorResourceProviderDefinition->setArgument(3, new \Symfony\Component\DependencyInjection\Reference('swp_multi_tenancy.tenant_context'));
+        }
     }
 }
