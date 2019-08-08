50677ab29e6e1757c60fe0ad918c8133b68bed6e
mehdi dridi <mehdignu@users.noreply.github.com>
2019-08-08 11:37:03 +0200

    [ADD] description to route

--- src/SWP/Bundle/CoreBundle/Migrations/2019/08/Version20190808092912.php
+++ src/SWP/Bundle/CoreBundle/Migrations/2019/08/Version20190808092912.php
@@ -1,18 +1,27 @@
 <?php
+
 declare(strict_types=1);
+
 namespace SWP\Migrations;
+
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+
 final class Version20190808092912 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(),
+            'Migration can only be executed safely on \'postgresql\'.');
+
         $this->addSql('ALTER TABLE swp_route ADD COLUMN description VARCHAR(255) DEFAULT NULL');
     }
+
     public function down(Schema $schema): void
     {
-        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(),
+            'Migration can only be executed safely on \'postgresql\'.');
+
         $this->addSql('ALTER TABLE swp_route COLUMN description');
     }
 }
