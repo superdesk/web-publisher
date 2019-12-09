c0f0a77f1718b4f57aee9b43f34c19d9c1b79524
Rafał Muszyński <rmuszynski1@gmail.com>
2019-12-09 08:56:39 +0100

    fixed tests

--- src/SWP/Behat/Service/GeoIPReaderAdapterStub.php
+++ src/SWP/Behat/Service/GeoIPReaderAdapterStub.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace SWP\Behat\Service;
 
 use SWP\Component\GeoIP\Reader\ReaderInterface;
+use GeoIp2\Exception\AddressNotFoundException;
 
 class GeoIPReaderAdapterStub implements ReaderInterface
 {
+    private const LOCALHOST = '127.0.0.1';
+
     /** @var string */
     private static $country = '';
 
@@ -16,11 +19,19 @@ class GeoIPReaderAdapterStub implements ReaderInterface
 
     public function getCountry(string $ipAddress): string
     {
+        if (self::LOCALHOST === $ipAddress) {
+            throw new AddressNotFoundException();
+        }
+
         return static::$country;
     }
 
     public function getState(string $ipAddress): string
     {
+        if (self::LOCALHOST === $ipAddress) {
+            throw new AddressNotFoundException();
+        }
+
         return static::$state;
     }
 
