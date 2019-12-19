c77ac76d2a2dccb87e20fa9361ed62c3e3745d77
Johan Larsson <johan.larsson@etc.se>
2019-12-19 07:59:58 +0100

    Update copyright year

--- src/SWP/Bundle/ContentBundle/Factory/S3ClientFactory.php
+++ src/SWP/Bundle/ContentBundle/Factory/S3ClientFactory.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 /*
  * This file is part of the Superdesk Web Publisher Content Bundle.
  *
- * Copyright 2016 Sourcefabric z.ú. and contributors.
+ * Copyright 2019 Sourcefabric z.ú. and contributors.
  *
  * For the full copyright and license information, please see the
  * AUTHORS and LICENSE files distributed with this source code.
  *
- * @copyright 2016 Sourcefabric z.ú
+ * @copyright 2019 Sourcefabric z.ú
  * @license http://www.superdesk.org/license
  */
 
