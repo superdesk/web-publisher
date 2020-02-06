d78a0adbd9cc245436bf8c990817e968c002da9e
Rafał Muszyński <rmuszynski1@gmail.com>
2020-02-06 11:08:59 +0100

    added migrations

---
+++ src/SWP/Bundle/CoreBundle/Migrations/2020/02/Version20200206100212.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200206100212 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_analytics_report ADD filters TEXT NOT NULL');
+        $this->addSql('COMMENT ON COLUMN swp_analytics_report.filters IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE swp_analytics_report DROP filters');
+    }
+}
