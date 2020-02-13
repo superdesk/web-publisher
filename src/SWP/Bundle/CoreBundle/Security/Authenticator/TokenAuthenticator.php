27477f899e23c73411bcb2b5533c502b9a3c6557
Rafał Muszyński <rmuszynski1@gmail.com>
2020-02-13 10:49:44 +0100

    fixes

--- src/SWP/Bundle/CoreBundle/Security/Authenticator/TokenAuthenticator.php
+++ src/SWP/Bundle/CoreBundle/Security/Authenticator/TokenAuthenticator.php
@@ -132,6 +132,23 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
 
     public function supports(Request $request): bool
     {
+//        $token = $this->getToken($request);
+//        $isApi = $request->attributes->get('_fos_rest_zone');
+//
+//        if (false === $isApi && $request->query->has('auth_token')) {
+//            return true;
+//        }
+//
+//        if (false === $isApi && null !== $token && false === strpos($token, 'Bearer ') && !$request->query->has('auth_token')) {
+//            return false;
+//        }
+//
+//        if (false === $isApi) {
+//            return false;
+//        }
+//
+//        return true;
+
         $token = $this->getToken($request);
         $isApi = $request->attributes->get('_fos_rest_zone');
 
@@ -139,15 +156,11 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
             return true;
         }
 
-        if (false === $isApi && null !== $token && false === strpos($token, 'Bearer ') && !$request->query->has('auth_token')) {
-            return false;
-        }
-
         if (false === $isApi) {
             return false;
         }
 
-        return true;
+        return null !== $token && false === strpos($token, 'Bearer ');
     }
 
     private function getToken(Request $request): ?string
