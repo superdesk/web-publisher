4db6507daf8b96609d888e5c281ac5efb2a74d20
mehdi <dridignu@gmail.com>
2020-04-09 16:08:02 +0200

    add compyright notice

--- src/SWP/Bundle/ContentBundle/Processor/EmbeddedImageProcessorInterface.php
+++ src/SWP/Bundle/ContentBundle/Processor/EmbeddedImageProcessorInterface.php
@@ -23,4 +23,6 @@ interface EmbeddedImageProcessorInterface extends ArticleBodyProcessorInterface
     public function setDefaultImageRendition(string $renditionName): void;
 
     public function applyByline(ArticleMediaInterface $articleMedia): string;
+
+    public function applyCopyrightNotice(ArticleMediaInterface $articleMedia): string;
 }
