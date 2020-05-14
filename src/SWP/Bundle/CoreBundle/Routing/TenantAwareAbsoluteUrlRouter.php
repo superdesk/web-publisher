ef8839ce9554ce9cc71079a286aabe90cd740dda
Rafał Muszyński <rmuszynski1@gmail.com>
2020-05-14 12:09:14 +0200

    added tenant aware router

--- src/SWP/Bundle/CoreBundle/Routing/TenantAwareAbsoluteUrlRouter.php
+++ src/SWP/Bundle/CoreBundle/Routing/TenantAwareAbsoluteUrlRouter.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Superdesk Web Publisher Core Bundle.
+ *
+ * Copyright 2020 Sourcefabric z.u. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2020 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
 namespace SWP\Bundle\CoreBundle\Routing;
 
 use SWP\Bundle\CoreBundle\Model\TenantInterface;
