8f320c6c5b11169d8be276e4a6fdf22bd3d26dc6
root <dridignu@gmail.com>
2020-08-24 18:44:33 +0200

    fix copyright notice varaible

--- src/SWP/Bundle/ContentBundle/Processor/EmbeddedImageProcessor.php
+++ src/SWP/Bundle/ContentBundle/Processor/EmbeddedImageProcessor.php
@@ -117,11 +117,11 @@ class EmbeddedImageProcessor implements EmbeddedImageProcessorInterface
     private function appendImageCopyrightNotice(ArticleMediaInterface $articleMedia, \DOMElement $figCaptionNode): void
     {
         $copyrightNotice = $this->applyCopyrightNotice($articleMedia);
-        if (null != copyrightNotice) {
+        if (null != $copyrightNotice) {
             $element = new \DOMElement('span');
             $figCaptionNode->appendChild($element);
             $authorDiv = $figCaptionNode->childNodes[1];
-            $authorDiv->textContent = copyrightNotice;
+            $authorDiv->textContent = $copyrightNotice;
         }
     }
 
