8c96e6fcc2f0fc96cea73f3e57168f6bd77b5e6d
Rafał Muszyński <rmuszynski1@gmail.com>
2020-09-01 15:52:55 +0200

    split logic into separate assets url generators

---
+++ src/SWP/Bundle/ContentBundle/Resolver/AssetLocationResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Superdesk Web Publisher Content Bundle.
+ *
+ * Copyright 2020 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2020 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
+namespace SWP\Bundle\ContentBundle\Resolver;
+
+use SWP\Bundle\ContentBundle\Asset\AssetUrlGeneratorInterface;
+use SWP\Bundle\ContentBundle\Model\FileInterface;
+
+class AssetLocationResolver implements AssetLocationResolverInterface
+{
+    private $assetUrlGenerator;
+
+    public function __construct(AssetUrlGeneratorInterface $assetUrlGenerator)
+    {
+        $this->assetUrlGenerator = $assetUrlGenerator;
+    }
+
+    public function getAssetUrl(FileInterface $file): string
+    {
+        return $this->assetUrlGenerator->generateUrl($file);
+    }
+
+    public function getMediaBasePath(): string
+    {
+        return AssetUrlGeneratorInterface::ASSET_BASE_PATH;
+    }
+}
