bb308c0daec96a05fffc7185f1f85a993d1c3cd5
Rafał Muszyński <rmuszynski1@gmail.com>
2021-01-04 10:18:59 +0100

    Run tests using github actions (#1174)
    * run tests on github actions
    
    * set fast-fail to false
    
    * added debug details
    
    * remove doctrine-data-fixtures-extension fork
    
    * update schema and fixtures command
    
    * postgres config
    
    * postgres config
    
    * improvements
    
    * fixed syntax
    
    * use sqlite for fixtures
    
    * use sqlite for fixtures
    
    * improvements
    
    * improvements
    
    * test only on ubuntu, skip behat tags
    
    * php unit tests
    
    * set non canonical bridge bundle repo
    
    * improve matrix
    
    * make repos non canonical
    
    * improve matrix
    
    * improve matrix
    
    * make repo non canonical
    
    * update deps, fixed tests
    
    * removed travis config, added Github Actions badge
    
    * update deps
    
    * fixed tests
    
    * fixed tests

--- src/SWP/Bundle/CoreBundle/Tests/ContentPushTest.php
+++ src/SWP/Bundle/CoreBundle/Tests/ContentPushTest.php
@@ -576,7 +576,7 @@ final class ContentPushTest extends WebTestCase
             $this->assertEquals(
                 [
                     'media_id' => $mediaId,
-                    'URL' => 'http://localhost/uploads/swp/123456/media/'.str_replace('/', '_', str_replace('jpg', 'jpeg', $mediaId)),
+                    'URL' => 'http://localhost/uploads/swp/123456/media/'.str_replace('/', '_', $mediaId),
                     'media' => base64_encode(file_get_contents(__DIR__.'/Functional/Resources/test_file.jpg')),
                     'mime_type' => 'image/jpeg',
                     'filemeta' => [],
@@ -611,8 +611,8 @@ final class ContentPushTest extends WebTestCase
         $client->request('GET', $this->router->generate('swp_package_preview', ['routeId' => 3, 'id' => 1, 'auth_token' => base64_encode('test_token:')]));
         self::assertEquals(200, $client->getResponse()->getStatusCode());
         $content = $client->getResponse()->getContent();
-        self::assertContains('<figure><img src="/uploads/swp/123456/media/20161206161256_383592fef7acb9fc4731a24a691285b7bc51477264a5e343d95c74ccf1d85a93.jpeg"', $content);
-        self::assertContains('alt="Article loaded from context" src="http://localhost/uploads/swp/123456/media/20161206161256_383592fef7acb9fc4731a24a691285b7bc51477264a5e343d95c74ccf1d85a93.jpeg"', $content);
+        self::assertContains('<figure><img src="/uploads/swp/123456/media/20161206161256_383592fef7acb9fc4731a24a691285b7bc51477264a5e343d95c74ccf1d85a93.jpg"', $content);
+        self::assertContains('alt="Article loaded from context" src="http://localhost/uploads/swp/123456/media/20161206161256_383592fef7acb9fc4731a24a691285b7bc51477264a5e343d95c74ccf1d85a93.jpg"', $content);
 
         // publish package
         $client->request(
