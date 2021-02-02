cfd8b24945481b13f66f102bd32b2083a2fc0a92
Rafał Muszyński <rmuszynski1@gmail.com>
2021-02-02 08:51:52 +0100

    add column if not exists

--- src/SWP/Bundle/CoreBundle/Migrations/2020/09/Version20200901105137.php
+++ src/SWP/Bundle/CoreBundle/Migrations/2020/09/Version20200901105137.php
@@ -17,14 +17,12 @@ final class Version20200901105137 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        //$this->addSql('ALTER TABLE swp_route ADD description VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE swp_route ADD COLUMN IF NOT EXISTS description VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
-
-        //$this->addSql('ALTER TABLE swp_route DROP description');
     }
 }
