40f370df216f4f842ebcb2eac0747a5a03f70c1d
Rafał Muszyński <rmuszynski1@gmail.com>
2021-02-09 14:16:42 +0100

    force retry when the lock is aquired (#1181)
    * force retry when the lock is aquired
    
    * retry on conflict
    
    * added lockFactory property
    
    * added missing exception class use
    
    * autowire lock factory
    
    * message handler service autowire

---
+++ src/SWP/Bundle/CoreBundle/MessageHandler/Exception/LockConflictedException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Superdesk Web Publisher Core Bundle.
+ *
+ * Copyright 2021 Sourcefabric z.ú. and contributors.
+ *
+ * For the full copyright and license information, please see the
+ * AUTHORS and LICENSE files distributed with this source code.
+ *
+ * @copyright 2021 Sourcefabric z.ú
+ * @license http://www.superdesk.org/license
+ */
+
+namespace SWP\Bundle\CoreBundle\MessageHandler\Exception;
+
+final class LockConflictedException extends \RuntimeException
+{
+}
