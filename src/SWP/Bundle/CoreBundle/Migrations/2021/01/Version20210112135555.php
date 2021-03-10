06219bb20381e70f82033ee896d4609bf2c4f420
Greg Szczotka <greg606@gmail.com>
2021-03-10 14:04:39 +0100

    add fix for a migration extra fields

--- src/SWP/Bundle/CoreBundle/Migrations/2021/01/Version20210112135555.php
+++ src/SWP/Bundle/CoreBundle/Migrations/2021/01/Version20210112135555.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200927180907 extends AbstractMigration
+final class Version20210112135555 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
