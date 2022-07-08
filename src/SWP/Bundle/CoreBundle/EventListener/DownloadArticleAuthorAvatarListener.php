6df02a0dc092110beba4eef4f8df79da1b19d946
WeaponMan <weaponman@centrum.cz>
2022-07-08 16:53:31 +0200

    Fix download of autors avatars

--- src/SWP/Bundle/CoreBundle/EventListener/DownloadArticleAuthorAvatarListener.php
+++ src/SWP/Bundle/CoreBundle/EventListener/DownloadArticleAuthorAvatarListener.php
@@ -16,6 +16,9 @@ declare(strict_types=1);
 
 namespace SWP\Bundle\CoreBundle\EventListener;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
 use Hoa\Mime\Mime;
 use SWP\Bundle\ContentBundle\Model\AuthorMediaInterface;
 use SWP\Bundle\CoreBundle\Model\Image;
@@ -30,107 +33,119 @@ use SWP\Bundle\ContentBundle\Model\ArticleAuthorInterface;
 use SWP\Bundle\ContentBundle\Manager\MediaManagerInterface;
 use SWP\Component\Common\Exception\UnexpectedTypeException;
 
-final class DownloadArticleAuthorAvatarListener
-{
-    /**
-     * @var MediaManagerInterface
-     */
-    private $authorMediaManager;
+final class DownloadArticleAuthorAvatarListener {
+  /**
+   * @var MediaManagerInterface
+   */
+  private $authorMediaManager;
 
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
+  /**
+   * @var EntityManagerInterface
+   */
+  private $entityManager;
 
-    /**
-     * @var string
-     */
-    private $cacheDirectory;
+  /**
+   * @var string
+   */
+  private $cacheDirectory;
 
-    public function __construct(EntityManagerInterface $entityManager, $cacheDirectory)
-    {
-        $this->entityManager = $entityManager;
-        $this->cacheDirectory = $cacheDirectory;
+  public function __construct(EntityManagerInterface $entityManager, $cacheDirectory) {
+    $this->entityManager = $entityManager;
+    $this->cacheDirectory = $cacheDirectory;
+  }
+
+  /**
+   * @param MediaManagerInterface $authorMediaManager
+   */
+  public function setAuthorMediaManager(MediaManagerInterface $authorMediaManager) {
+    $this->authorMediaManager = $authorMediaManager;
+  }
+
+  /**
+   * @param GenericEvent $event
+   */
+  public function processAuthors(GenericEvent $event): void {
+    $package = $event->getSubject();
+
+    if (!$package instanceof PackageInterface) {
+      throw new UnexpectedTypeException($package, PackageInterface::class);
     }
 
-    /**
-     * @param MediaManagerInterface $authorMediaManager
-     */
-    public function setAuthorMediaManager(MediaManagerInterface $authorMediaManager)
-    {
-        $this->authorMediaManager = $authorMediaManager;
+    $authors = [];
+    /** @var ArticleAuthorInterface $packageAuthor */
+    foreach ($package->getAuthors() as $packageAuthor) {
+      $authors[] = $this->handle($packageAuthor);
     }
+    $package->setAuthors(new ArrayCollection($authors));
+  }
 
-    /**
-     * @param GenericEvent $event
-     */
-    public function processAuthors(GenericEvent $event): void
-    {
-        $package = $event->getSubject();
-
-        if (!$package instanceof PackageInterface) {
-            throw new UnexpectedTypeException($package, PackageInterface::class);
-        }
-
-        $authors = [];
-        /** @var ArticleAuthorInterface $packageAuthor */
-        foreach ($package->getAuthors() as $packageAuthor) {
-            $authors[] = $this->handle($packageAuthor);
-        }
-        $package->setAuthors(new ArrayCollection($authors));
-    }
-
-    /**
-     * @param ArticleAuthorInterface $object
-     *
-     * @return ArticleAuthorInterface
-     */
-    private function handle(ArticleAuthorInterface $object): ArticleAuthorInterface
-    {
-        if (null !== $object->getAvatarUrl()) {
-            $filesystem = new Filesystem();
-            $pathParts = \pathinfo($object->getAvatarUrl());
-            $assetId = \sha1($pathParts['filename']);
-            if (null !== $object->getSlug()) {
-                $assetId = $object->getSlug().'_'.$assetId;
-            }
-            $existingAvatar = $this->entityManager->getRepository(Image::class)->findBy(['assetId' => $assetId]);
-            if (\count($existingAvatar) > 0) {
-                $object->setAvatarUrl((string) \reset($existingAvatar));
-
-                return $object;
-            }
-
-            try {
-                $file = \file_get_contents($object->getAvatarUrl());
-                if (false === $file) {
-                    throw new \Exception('File can\'t be downloaded');
-                }
-                $tempDirectory = $this->cacheDirectory.\DIRECTORY_SEPARATOR.'downloaded_avatars';
-                $tempLocation = $tempDirectory.\DIRECTORY_SEPARATOR.\sha1($assetId.date('his'));
-                if (!$filesystem->exists($tempDirectory)) {
-                    $filesystem->mkdir($tempDirectory);
-                }
-                $filesystem->dumpFile($tempLocation, $file);
-                $uploadedFile = new UploadedFile($tempLocation, $assetId, Mime::getMimeFromExtension($pathParts['extension']), \strlen($file), null, true);
-            } catch (\Exception $e) {
-                return $object;
-            }
-            /** @var Image $image */
-            $image = $this->authorMediaManager->handleUploadedFile($uploadedFile, $assetId);
-            $avatar = $this->createAuthorMedia($object, $image);
-            $this->entityManager->persist($avatar);
-            $this->entityManager->persist($image);
-
-            $object->setAvatar($avatar);
-            $object->setAvatarUrl((string) $image);
-        }
+  /**
+   * @param ArticleAuthorInterface $object
+   *
+   * @return ArticleAuthorInterface
+   */
+  private function handle(ArticleAuthorInterface $object): ArticleAuthorInterface {
+    if (null !== $object->getAvatarUrl()) {
+      $filesystem = new Filesystem();
+      $pathParts = \pathinfo($object->getAvatarUrl());
+      $assetId = \sha1($pathParts['filename']);
+      if (null !== $object->getSlug()) {
+        $assetId = $object->getSlug() . '_' . $assetId;
+      }
+      $existingAvatar = $this->entityManager->getRepository(Image::class)->findBy(['assetId' => $assetId]);
+      if (\count($existingAvatar) > 0) {
+        $object->setAvatarUrl((string)\reset($existingAvatar));
 
         return $object;
+      }
+
+      try {
+        $tempDirectory = $this->cacheDirectory . \DIRECTORY_SEPARATOR . 'downloaded_avatars';
+        $tempLocation = $tempDirectory . \DIRECTORY_SEPARATOR . \sha1($assetId . date('his'));
+        if (!$filesystem->exists($tempDirectory)) {
+          $filesystem->mkdir($tempDirectory);
+        }
+
+        $avatarUrl = $object->getAvatarUrl();
+        if (strpos($avatarUrl, "http://") === 0 || strpos($avatarUrl, "https://") === 0) {
+          self::downloadFile($avatarUrl, $tempLocation);
+        } else {
+          $file = \file_get_contents($avatarUrl);
+          if (false === $file) {
+            throw new \Exception('File can\'t be downloaded');
+          }
+          $filesystem->dumpFile($tempLocation, $file);
+        }
+
+        $uploadedFile = new UploadedFile($tempLocation,
+            $assetId,
+            Mime::getMimeFromExtension($pathParts['extension']),
+            \filesize($tempLocation),
+            true
+        );
+      } catch (\Exception $e) {
+        return $object;
+      }
+      /** @var Image $image */
+      $image = $this->authorMediaManager->handleUploadedFile($uploadedFile, $assetId);
+      $avatar = $this->createAuthorMedia($object, $image);
+      $this->entityManager->persist($avatar);
+      $this->entityManager->persist($image);
+
+      $object->setAvatar($avatar);
+      $object->setAvatarUrl((string)$image);
     }
 
-    private function createAuthorMedia(ArticleAuthorInterface $articleAuthor, Image $image): AuthorMediaInterface
-    {
-        return new AuthorMedia('avatar', $articleAuthor, $image);
-    }
+    return $object;
+  }
+
+  private function createAuthorMedia(ArticleAuthorInterface $articleAuthor, Image $image): AuthorMediaInterface {
+    return new AuthorMedia('avatar', $articleAuthor, $image);
+  }
+
+  private static function downloadFile($url, $location) {
+    $handlerStack = HandlerStack::create(new CurlHandler());
+    $client = new Client(['handler' => $handlerStack]);
+    $client->request('GET', $url, ['sink' => $location]);
+  }
 }
