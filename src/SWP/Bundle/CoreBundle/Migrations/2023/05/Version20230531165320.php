d2b11fd0ea1aeede47ac3722d9dc1ce3ba74bb8e
IvanJelicSF <ivan.jelic@sourcefabric.org>
2023-05-31 16:59:43 +0200

    PWA column migration added

---
+++ src/SWP/Bundle/CoreBundle/Migrations/2023/05/Version20230531165320.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SWP\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230531165320 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+        $this->addSql('ALTER TABLE swp_tenant ADD COLUMN IF NOT EXISTS pwa_config_url varchar(255) NULL DEFAULT NULL::character varying');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+    }
+}
